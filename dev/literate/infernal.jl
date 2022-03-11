a50f2a07c6eac13f20ef576dce4971a7f171c4cb
Documenter.jl <documenter@juliadocs.github.io>
2022-03-11 01:26:22 +0000

    build based on 753e68c

--- dev/literate/infernal.jl
+++ dev/literate/infernal.jl
@@ -3,3 +3,12 @@
 =#
 
 import Infernal
+import Rfam
+
+# Retrieve the path to Infernal programs
+
+isfile(Infernal.path("cmalign"))
+
+# Execute a command
+
+success(Infernal.exe("cmfetch", Rfam.cm(), "RF00162"))
