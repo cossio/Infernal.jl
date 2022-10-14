55f45b3be00bddb4b6aa4d399082696ee9659576
cossio <cossio@users.noreply.github.com>
2022-10-14 10:13:47 +0200

    cmsearch strands

--- src/cmsearch.jl
+++ src/cmsearch.jl
@@ -1,9 +1,17 @@
-function cmsearch(cmfile::AbstractString, seqfile::AbstractString)
+function cmsearch(
+    cmfile::AbstractString, seqfile::AbstractString;
+    toponly::Bool=false, bottomonly::Bool=false
+)
     exe = infernal_binary("cmsearch")
     cmd = `$exe`
+
+    toponly && (cmd = `$cmd --toponly`)
+    bottomonly && (cmd = `$cmd --bottomonly`)
+
     out = tempname()
     stdout = tempname()
     stderr = tempname()
+
     run(pipeline(`$cmd -o $out $cmfile $seqfile`; stdout, stderr, append=false))
     return (; out, stdout, stderr)
 end
