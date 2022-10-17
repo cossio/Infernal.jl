5e9a8b9c829d34624ab0cf4af923276b20b1e938
cossio <cossio@users.noreply.github.com>
2022-10-18 00:38:47 +0200

    cmbuild o, O

--- src/cmbuild.jl
+++ src/cmbuild.jl
@@ -1,15 +1,17 @@
 function cmbuild(
     msafile::AbstractString;
-    enone::Bool=false,
+    enone::Bool=false
 )
     exe = infernal_binary("cmbuild")
     cmd = `$exe`
     enone && (cmd = `$cmd --enone`)
 
-    cmout = tempname()
     stdout = tempname()
     stderr = tempname()
+    cmout = tempname()
+    o = tempname()
+    O = tempname()
 
-    run(pipeline(`$cmd $cmout $msafile`; stdout, stderr, append=false))
-    return (; cmout, stdout, stderr)
+    run(pipeline(`$cmd -o $o -O $O $cmout $msafile`; stdout, stderr, append=false))
+    return (; cmout, stdout, stderr, o, O)
 end
