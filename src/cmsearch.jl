562c8fd56f7d890c8aa6af94dfe5ec01d00718f5
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-11-02 11:43:11 +0100

    cmsearch_parse_tblout

--- src/cmsearch.jl
+++ src/cmsearch.jl
@@ -23,3 +23,16 @@ function cmsearch(
     )
     return (; out, stdout, stderr, A, tblout)
 end
+
+function cmsearch_parse_tblout(tblout::AbstractString)
+    csv = CSV.File(
+        tblout; delim=' ', ignorerepeated=true, comment="#",
+        header=[
+            "target_name", "accession", "query_name", "accession2",
+            "mdl", "mdl_from", "mdl_to", "seq_from", "seq_to", "strand",
+            "trunc", "pass", "gc", "bias", "score",
+            "E_value", "inc", "description_of_target"
+        ]
+    )
+    return DataFrame(csv)
+end
