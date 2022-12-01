9f7b37afe7caa6adf5a4ff43fd752d2ce83598a8
cossio <j.cossio.diaz@gmail.com>
2022-12-01 15:48:07 +0100

    use Rfam in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,32 +1,25 @@
 using Test: @test, @testset, @inferred
 using Downloads: download
 import Gzip_jll
+import Rfam
 
 # decompress a gzipped file
 gunzip(file::String) = run(`$(Gzip_jll.gzip()) -d $file`)
 
 const RFAM_ID = "RF00162" # Family we will use in tests
 const RFAM_VERSION = "14.8"
+const RFAM_DIR = tempdir()
 
 @info "Downloading Rfam.cm ..."
-const CM_FILE = tempname()
-download("https://ftp.ebi.ac.uk/pub/databases/Rfam/14.8/Rfam.cm.gz", CM_FILE * ".gz")
-gunzip(CM_FILE * ".gz")
+const CM_FILE = Rfam.cm(; dir=RFAM_DIR, version=RFAM_VERSION)
 @test isfile(CM_FILE)
 
 @info "Downloading all seed alignments ..."
-const RFAM_SEED = tempname()
-download("https://ftp.ebi.ac.uk/pub/databases/Rfam/$RFAM_VERSION/Rfam.seed.gz", RFAM_SEED * ".gz")
-gunzip(RFAM_SEED * ".gz")
+const RFAM_SEED = Rfam.seed(; dir=RFAM_DIR, version=RFAM_VERSION)
 @test isfile(RFAM_SEED)
 
 @info "Downloading $RFAM_ID hit sequences ..."
-const FULL_FASTA_FILE = tempname()
-download(
-    "https://ftp.ebi.ac.uk/pub/databases/Rfam/$RFAM_VERSION/fasta_files/$RFAM_ID.fa.gz",
-    FULL_FASTA_FILE * ".gz"
-)
-gunzip(FULL_FASTA_FILE * ".gz")
+const FULL_FASTA_FILE = Rfam.fasta_file(RFAM_ID; dir=RFAM_DIR, version=RFAM_VERSION)
 @test isfile(FULL_FASTA_FILE)
 
 # Tests
