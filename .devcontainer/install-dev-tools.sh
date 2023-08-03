5363768e6c7fa4d1902c28bf039e705039a41f04
cossio <cossio@users.noreply.github.com>
2023-08-03 21:16:51 +0000

    unattended install of rye

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -2,5 +2,5 @@
 curl -fsSL https://install.julialang.org | sh -s -- --yes
 
 # rye
-curl -sSf https://rye-up.com/get | bash -s -- --yes
+curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
 echo 'source "$HOME/.rye/env"' > .profile
